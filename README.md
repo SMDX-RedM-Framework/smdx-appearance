614b332379e5caf274c7a0df713228e5af4c44f9
SMDX Development <emilsven2021@gmail.com>
2024-06-28 21:34:19 +0200

    Created clothes.lua

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # smdx-appearance
- 
+
+# Dependencies
+- smdx-core
+- ox_lib
+
+# Links
+- ox_lib download: https://github.com/overextended/ox_lib/releases/tag/v3.22.2
+- oxmysql download: https://github.com/overextended/oxmysql/releases/tag/v2.10.1
+
+# Discord
+- SMDX RedM Framework Discord community: https://discord.gg/FuqTqqreYV
+
+# credits
+- original resouce created by : https://github.com/QRCore-RedM-Re
+
+# This version is reworked by SMDX Development
+# 2024
---
+++ fxmanifest.lua
@@ -0,0 +1,36 @@
+fx_version 'cerulean'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+game 'rdr3'
+lua54 'yes'
+use_experimental_fxv2_oal 'yes'
+
+description 'smdx-appearance'
+version '1.0.0'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'config.lua'
+}
+
+client_scripts {
+    'client/*.lua',
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server/*.lua',
+}
+
+files {
+    'img/*.png',
+    'data/features.lua',
+    'data/overlays.lua',
+    'data/clothing.lua',
+    'data/hairs_list.lua',
+    'data/clothes_list.lua'
+}
+
+dependencies {
+    'smdx-core',
+    'ox_lib'
+}
